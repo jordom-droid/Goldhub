27871b258ff10ba4906ec6b861c79470e4c973ff
jordom-droid <jordom@gmail.com>
2026-09-23 20:52:43 +1000

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,1 @@
-self.addEventListener('install',e=>self.skipWaiting());self.addEventListener('activate',e=>self.registration.unregister());
+const CACHE='goldwindow-v2'; self.addEventListener('install',e=>self.skipWaiting()); self.addEventListener('activate',e=>e.waitUntil(self.clients.claim())); self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return; e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));});
